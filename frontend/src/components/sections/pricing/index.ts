72b5cc25dcab919ca745b9d8c00223ddbffa9248
samuelbrhanealemayohu <contact@samuelbrhane.com>
2025-12-27 11:55:13 +0300

    feat: implement Pricing component with billing cycle management and section exports

---
+++ frontend/src/components/sections/pricing/index.ts
@@ -0,0 +1,4 @@
+export { default as PricingHero } from "./Pricinghero";
+export { default as PricingTiers } from "./Pricingtiers";
+export { default as PricingFAQ } from "./Pricingfaq";
+export { default as PricingCTA } from "./Pricingcta";
