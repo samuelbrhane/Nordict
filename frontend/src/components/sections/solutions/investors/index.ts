dea91b82a84aee252013c1766065500bf8557a91
samuelbrhanealemayohu <contact@samuelbrhane.com>
2025-12-27 11:22:41 +0300

    feat: implement Investors page with components for user engagement and investment guidance

---
+++ frontend/src/components/sections/solutions/investors/index.ts
@@ -0,0 +1,5 @@
+export { default as InvestorsHero } from "./Investorshero";
+export { default as InvestmentApproach } from "./Investmentapproach";
+export { default as InvestorFeatures } from "./Investorfeatures";
+export { default as InvestorUseCases } from "./Investorusecases";
+export { default as InvestorTestimonial } from "./Investortestimonial";
