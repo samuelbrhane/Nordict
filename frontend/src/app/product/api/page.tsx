547d8fc7bfaca12e66169e44d9db308e6fe05ddc
samuelbrhanealemayohu <contact@samuelbrhane.com>
2025-12-27 09:57:26 +0300

    feat: add SDKs component to API page and export from index

--- frontend/src/app/product/api/page.tsx
+++ frontend/src/app/product/api/page.tsx
@@ -2,6 +2,7 @@ import {
   APIHero,
   Authentication,
   EndpointsOverview,
+  SDKs,
   Webhooks,
 } from "@/components/sections/product/api";
 
@@ -12,6 +13,7 @@ const Api = () => {
       <EndpointsOverview />
       <Authentication />
       <Webhooks />
+      <SDKs />
     </div>
   );
 };
--- frontend/src/components/sections/product/api/index.ts
+++ frontend/src/components/sections/product/api/index.ts
@@ -2,3 +2,4 @@ export { default as APIHero } from "./Apihero";
 export { default as EndpointsOverview } from "./Endpointsoverview";
 export { default as Authentication } from "./ApiAuthentication";
 export { default as Webhooks } from "./Webhooks";
+export { default as SDKs } from "./Sdks";
