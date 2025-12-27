d1f8d28d4310de4779912853453026a3cc9043db
samuelbrhanealemayohu <contact@samuelbrhane.com>
2025-12-27 11:13:31 +0300

    feat: add Traders section exports for improved component organization

---
+++ frontend/src/components/sections/solutions/traders/index.ts
@@ -0,0 +1,5 @@
+export { default as TradersHero } from "./Tradershero";
+export { default as TradingWorkflow } from "./Tradingworkflow";
+export { default as TraderFeatures } from "./Traderfeatures";
+export { default as TraderUseCases } from "./Traderusecases";
+export { default as TraderTestimonial } from "./Tradertestimonial";
