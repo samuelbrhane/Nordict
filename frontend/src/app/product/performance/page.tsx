b3d75a17ef76919b43c871eef840d29488984867
samuelbrhanealemayohu <contact@samuelbrhane.com>
2025-12-27 08:54:30 +0300

    feat: add Performance component and PerformanceHero export for product performance section

---
+++ frontend/src/app/product/performance/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { PerformanceHero } from "@/components/sections/product/performance";
+
+const Performance = () => {
+  return (
+    <div>
+      <PerformanceHero />
+    </div>
+  );
+};
+
+export default Performance;
