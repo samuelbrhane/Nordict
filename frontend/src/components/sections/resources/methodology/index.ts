fc260979403e99323cf086ff722b5db1d1ebac5a
samuelbrhanealemayohu <contact@samuelbrhane.com>
2025-12-27 12:23:18 +0300

    feat: add index file for Methodology components to streamline imports

---
+++ frontend/src/components/sections/resources/methodology/index.ts
@@ -0,0 +1,8 @@
+import MethodologyCTA from "./Methodologycta";
+export { default as MethodologyHero } from "./Methodologyhero";
+export { default as ModelOverview } from "./Modeloverview";
+export { default as DataSources } from "./Datasources";
+export { default as ForecastProcess } from "./Forecastprocess";
+export { default as Validation } from "./Validation";
+export { default as Limitations } from "./Limitations";
+export { default as MethodologyCTA } from "./Methodologycta";
