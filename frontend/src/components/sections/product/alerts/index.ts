bd1a80b3c45287113e951420cb8468e21726b11a
samuelbrhanealemayohu <contact@samuelbrhane.com>
2025-12-27 09:39:04 +0300

    feat: add index file for alert components export

---
+++ frontend/src/components/sections/product/alerts/index.ts
@@ -0,0 +1,6 @@
+export { default as AlertsHero } from "./Alertshero";
+export { default as AlertTypes } from "./Alerttypes";
+export { default as TriggerLogic } from "./Triggerlogic";
+export { default as DeliveryChannels } from "./Deliverychannels";
+export { default as AlertExamples } from "./Alertexamples";
+export { default as AlertNoise } from "./Alertnoise";
