ebc4ff8c7d571fb79c12d89b8dbff6b8d89d230e
samuelbrhanealemayohu <contact@samuelbrhane.com>
2025-12-27 08:19:54 +0300

    feat: export AlertsSignals component in product section

--- frontend/src/components/sections/product/index.ts
+++ frontend/src/components/sections/product/index.ts
@@ -2,3 +2,4 @@ export { default as ProductHero } from "./ProductHero";
 export { default as ForecastingCapabilities } from "./Forecastingcapabilities";
 export { default as ConfidenceUncertainty } from "./Confidenceuncertainty";
 export { default as BacktestingPerformance } from "./Backtestingperformance";
+export { default as AlertsSignals } from "./Alertssignals";
