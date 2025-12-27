1f5624e5e5b7b54f68629cab1fd85b5bb44aa9b7
samuelbrhanealemayohu <contact@samuelbrhane.com>
2025-12-27 08:15:26 +0300

    feat: export ConfidenceUncertainty and BacktestingPerformance components in product section

--- frontend/src/components/sections/product/index.ts
+++ frontend/src/components/sections/product/index.ts
@@ -1,2 +1,4 @@
 export { default as ProductHero } from "./ProductHero";
 export { default as ForecastingCapabilities } from "./Forecastingcapabilities";
+export { default as ConfidenceUncertainty } from "./Confidenceuncertainty";
+export { default as BacktestingPerformance } from "./Backtestingperformance";
