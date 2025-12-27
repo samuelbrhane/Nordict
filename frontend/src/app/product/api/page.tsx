63d40d3450b19fe50fc717fa489581eb932ac666
samuelbrhanealemayohu <contact@samuelbrhane.com>
2025-12-27 09:47:54 +0300

    feat: add Api component and APIHero export for product API section

---
+++ frontend/src/app/product/api/page.tsx
@@ -0,0 +1,11 @@
+import { APIHero } from "@/components/sections/product/api";
+
+const Api = () => {
+  return (
+    <div>
+      <APIHero />
+    </div>
+  );
+};
+
+export default Api;
---
+++ frontend/src/components/sections/product/api/index.ts
@@ -0,0 +1,1 @@
+export { default as APIHero } from "./Apihero";
