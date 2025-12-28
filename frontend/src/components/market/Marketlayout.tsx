550125bc2955f1247ae34d971a08c11a484f1cd9
samuelbrhanealemayohu <contact@samuelbrhane.com>
2025-12-28 08:41:20 +0300

    feat: add MarketingLayout component with Header and Footer integration

---
+++ frontend/src/components/market/Marketlayout.tsx
@@ -0,0 +1,15 @@
+import { Header, Footer } from ".";
+
+export default function MarketingLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <>
+      <Header />
+      {children}
+      <Footer />
+    </>
+  );
+}
