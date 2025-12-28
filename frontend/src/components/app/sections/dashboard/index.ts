13c3c29f8bef3cdc180e1836dc4e6510cfd32ca7
samuelbrhanealemayohu <contact@samuelbrhane.com>
2025-12-28 11:49:37 +0300

    feat: add ForecastChart component for market price forecasting

---
+++ frontend/src/components/app/sections/dashboard/index.ts
@@ -0,0 +1,6 @@
+export { default as AnimatedCard } from "./AnimatedCard";
+export { default as DashboardHeader } from "./DashboardHeader";
+export { default as KpiTiles } from "./KpiTiles";
+export { default as ForecastChart } from "./ForecastChart";
+export { default as TopSignalsTable } from "./TopSignalsTable";
+export { default as DeltaFeed } from "./DeltaFeed";
