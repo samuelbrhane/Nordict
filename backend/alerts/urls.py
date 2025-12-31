45998bab2279e243efb82e6144665973fa980ca3
samuelbrhanealemayohu <contact@samuelbrhane.com>
2025-12-31 18:15:42 +0300

    refactor: remove alerts app and related files

--- backend/alerts/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.AlertListCreateView.as_view(), name='alert-list'),
-    path('<int:pk>/', views.AlertDetailView.as_view(), name='alert-detail'),
-    path('<int:pk>/history/', views.AlertHistoryView.as_view(), name='alert-history'),
-]
