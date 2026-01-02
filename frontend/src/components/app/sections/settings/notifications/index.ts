697fbff13e476dfbf5ebeed9273b8a4bd2f7a611
samuelbrhanealemayohu <contact@samuelbrhane.com>
2026-01-02 20:28:13 +0300

    refactor: remove DigestSettings component from notifications index

--- frontend/src/components/app/sections/settings/notifications/index.ts
+++ frontend/src/components/app/sections/settings/notifications/index.ts
@@ -3,5 +3,4 @@ export { default as NotificationsHeader } from "./NotificationsHeader";
 export { default as AlertChannels } from "./AlertChannels";
 export { default as ForecastUpdates } from "./ForecastUpdates";
 export { default as SystemNotifications } from "./SystemNotifications";
-export { default as DigestSettings } from "./DigestSettings";
 export { default as QuietHours } from "./QuietHours";
