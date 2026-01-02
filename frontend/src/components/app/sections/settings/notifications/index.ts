3b4e513686298dc12af158ae617c5dde5fb084aa
samuelbrhanealemayohu <contact@samuelbrhane.com>
2026-01-02 20:44:43 +0300

    refactor: remove QuietHours and SystemNotifications components from notifications index

--- frontend/src/components/app/sections/settings/notifications/index.ts
+++ frontend/src/components/app/sections/settings/notifications/index.ts
@@ -2,5 +2,3 @@ export { default as Toggle } from "./ToggleRow";
 export { default as NotificationsHeader } from "./NotificationsHeader";
 export { default as AlertChannels } from "./AlertChannels";
 export { default as ForecastUpdates } from "./ForecastUpdates";
-export { default as SystemNotifications } from "./SystemNotifications";
-export { default as QuietHours } from "./QuietHours";
